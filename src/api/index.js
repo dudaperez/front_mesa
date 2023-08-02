2b2a0425b7133ec956a305892db344152908e876
Eduardo Perez <eduardopcwb@gmail.com>
2023-08-02 00:57:25 -0300

    API dinamica

--- src/api/index.js
+++ src/api/index.js
@@ -27,31 +27,42 @@ import axios from 'axios';
 
 const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
 
-const options = {
-  params: {
-    bl_latitude: '11.847676',
-    tr_latitude: '12.838442',
-    bl_longitude: '109.095887',
-    tr_longitude: '109.149359',
-    // restaurant_tagcategory_standalone: '10591',
-    // restaurant_tagcategory: '10591',
-    // limit: '30',
-    // currency: 'USD',
-    // open_now: 'false',
-    // lunit: 'km',
-    // lang: 'en_US'
-  },
-  headers: {
-    'X-RapidAPI-Key': '0299537a4bmsha0461f7f0b74ed2p1f6d30jsn5663d4fd4dcd',
-    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-  }
-};
+// const options = {
+//   params: {
+//     bl_latitude: '11.847676',
+//     tr_latitude: '12.838442',
+//     bl_longitude: '109.095887',
+//     tr_longitude: '109.149359',
+//     // restaurant_tagcategory_standalone: '10591',
+//     // restaurant_tagcategory: '10591',
+//     // limit: '30',
+//     // currency: 'USD',
+//     // open_now: 'false',
+//     // lunit: 'km',
+//     // lang: 'en_US'
+//   },
+//   headers: {
+//     'X-RapidAPI-Key': '0299537a4bmsha0461f7f0b74ed2p1f6d30jsn5663d4fd4dcd',
+//     'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+//   }
+// };
 
 
 
-export const getPlacesData = async () => {
+export const getPlacesData = async (sw, ne) => {
     try{
-        const {data: { data } } = await axios.get(URL, options);
+        const {data: { data } } = await axios.get(URL, {
+          params: {
+            bl_latitude: sw.lat ,
+            tr_latitude: ne.lat,
+            bl_longitude: sw.lng,
+            tr_longitude: ne.lng,
+          },
+          headers: {
+            'X-RapidAPI-Key': '0299537a4bmsha0461f7f0b74ed2p1f6d30jsn5663d4fd4dcd',
+            'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+          }
+        });
 
         return data;
 
