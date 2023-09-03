69bb5be07931b70ef3a15e720b0bc7efab17ee7d
Eduardo Perez <eduardopcwb@gmail.com>
2023-09-03 17:50:48 -0300

    Botao para adicionar na lista

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core';
+import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip} from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import PhoneIcon from '@material-ui/icons/Phone';
 import Rating from '@material-ui/lab/Rating';
 import useStyles from './styles';
-
+import { ToggleButton } from '@material-ui/lab';
 
 
 
 const PlaceDetails = ({place}) => {
     const classes = useStyles();
+    const [selected, setSelected] = React.useState(false)
 
     return (
-        <Card elevation={6}>
+        <Card elevation={6}>            
             <CardMedia
                 style={{height:200}}
                 image={place.photo? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
@@ -22,6 +24,15 @@ const PlaceDetails = ({place}) => {
             <CardContent>
                 <Typography gutterBottom variant="h5">{place.name}</Typography>
                 <Box display = "flex" justifyContent="space-between">
+                <ToggleButton
+                    value="Adicionar"
+                    selected={selected}
+                    onChange={() => {
+                        setSelected(!selected);
+                    }}
+                >
+                    <AddIcon />
+                </ToggleButton>     
                     <Typography variant="subtitle1">Preço</Typography>
                     <Typography gutterBottom variant="subtitle1">{place.price_level}</Typography>
                 </Box>
